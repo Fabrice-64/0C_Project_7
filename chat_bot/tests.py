a48c109cf936876e56124912c2f479532e734a94
Fabrice-64 <fab.jaouen@gmail.com>
2020-07-01 18:55:01 +0200

    preparing the set up

--- chat_bot/tests.py
+++ chat_bot/tests.py
@@ -7,7 +7,16 @@ class Test(unittest.TestCase):
         response = requests.get('http://localhost:5000')
         assert response.status_code == 200
 
+    def test_enter_search(self):
+        self.question1 = "Bonjour, parle-moi de la Cité Paradis, à Paris"
+        self.assertEqual(self.question1.enter_search,
+                         "Bonjour, parle-moi de la Cité Paradis, à Paris")
 
+    def test_search_parsing(self):
+        self.question1 = "Bonjour, parle-moi de la Cité Paradis, à Paris?"
+        self.question2 = "Bonjour, qu'il y a-t-il Rue de Rivoli, à Paris?"
+        self.assertEqual(self.question1.enter_search,
+                         {'street': 'Cité Paradis', 'city': 'Paris'})
 
 
 if __name__ == '__main__':
