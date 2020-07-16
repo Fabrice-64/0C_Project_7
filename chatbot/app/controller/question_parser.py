fa6173f964dc02c221a7f4e8fa8758cd9ecb9cd6
Fabrice-64 <fab.jaouen@gmail.com>
2020-07-16 14:45:48 +0200

    Structuring the project with new folders dedicated to parsing

---
+++ chatbot/app/controller/question_parser.py
@@ -0,0 +1,8 @@
+
+question_to_parse = []
+
+
+class QuestionParser():
+    def parse_question(self, question_to_parse):
+        pass
+
--- chatbot/app/routes.py
+++ chatbot/app/routes.py
@@ -1,10 +1,14 @@
 from flask import render_template, flash, request
 from app import app
 from app.forms import QuestionForm
-
+from app.controller import question_parser
 
 @app.route('/')
 @app.route('/interface', methods=['GET', 'POST'])
 def interface():
     question = QuestionForm()
+    if request.method == 'POST' and question.validate():
+        flash('La question est: {}'.format(question.question.data))
+        question_parser.question_to_parse.append(question.question.data)
+        print('Response: {}'.format(question_parser.question_to_parse[0]))
     return render_template('interface.html', title='Home', question=question)
