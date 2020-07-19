70b7f0deddd6e3ef36f13ca889e396a1bebe9961
Fabrice-64 <fab.jaouen@gmail.com>
2020-07-19 10:59:30 +0200

    implement mark.parametrize in parser_test.py

--- chatbot/tests/conftest.py
+++ chatbot/tests/conftest.py
@@ -8,3 +8,12 @@ class Path():
     PATH_TO_JAR = ROOT + JAR
     PATH_TO_MODEL = ROOT+MODEL
 
+
+class TestParameters():
+    questions = [
+        ('Bonjour ChatBot, pourrais-tu me parler des Champs-Elysées,\
+        à Paris ? Bisous à tout le monde', 'Champs-Elysées Paris'),
+        ('Coucou mon Papounet, toi qui sais tout, c\'est quoi,\
+        la Vallée des Saints ?', 'Vallée Saints'),
+        ('Salut Fab, tu connais le musée des Invalides?', 'Musée Invalides')
+    ]
--- chatbot/tests/parser_test.py
+++ chatbot/tests/parser_test.py
@@ -1,25 +1,16 @@
-from nltk.internals import find_jars_within_path
 from nltk.tag import StanfordPOSTagger
 from nltk import word_tokenize
-from tests.conftest import Path
-
-# Alternatively to setting the CLASSPATH add the jar and model via their path:
-original_question = 'Bonjour ChatBot, pourrais-tu me parler des Champs-Elysées,\
-    à Paris ? Bisous à tout le monde'
-expected_answer = 'Champs-Elysées'
+from pytest import mark
+from tests.conftest import Path, TestParameters
 
 
+def tag_words(model, jar, original_question):
+    pos_tagger = StanfordPOSTagger(model, jar, encoding="utf-8")
+    tagged_words = pos_tagger.tag(word_tokenize(original_question))
+    return tagged_words
 
 
-def func_pos_tagger(model, jar, original_question):
-    pos_tagger = StanfordPOSTagger(model, jar)
-    # Add other jars from Stanford directory
-    stanford_dir = pos_tagger._stanford_jar.rpartition('/')[0]
-    stanford_jars = find_jars_within_path(stanford_dir)
-    pos_tagger._stanford_jar = ':'.join(stanford_jars)
-    res = pos_tagger.tag(word_tokenize(original_question))
-    return res
-
-
-def test_question_parser():
-    assert func_pos_tagger(Path.PATH_TO_MODEL, Path.PATH_TO_JAR, original_question) == expected_answer
+@mark.parametrize("test_input, expected", TestParameters.questions)
+def test_question_parser(test_input, expected):
+    assert tag_words(Path.PATH_TO_MODEL, Path.PATH_TO_JAR, test_input
+                     ) == expected
