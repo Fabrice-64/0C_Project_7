7eae188e4d9a467f9045286853f43be6e7311a51
Fabrice-64 <fab.jaouen@gmail.com>
2020-07-25 15:16:26 +0200

    Optimizing conftest.py

--- chatbot/tests/conftest.py
+++ chatbot/tests/conftest.py
@@ -1,35 +1,42 @@
 
 
 class TestParameters():
-    questions_tokenize = [
-        ('Bonjour ChatBot, pourrais-tu me parler des Champs-Elysées, à Paris ? Bisous à tout le monde', 
-        'parler Champs Elysées Paris ? monde'),
-        ('Coucou mon Papounet, toi qui sais tout, c\'est quoi, la Vallée des Saints ?',
-        'sais Vallée Saints ?'),
-        ('Salut Fab, tu connais le Musée des Invalides?',
-        'connais Musée Invalides ?')
+    question1 = [
+        'Bonjour ChatBot, pourrais-tu me parler des Champs-Elysées, à Paris ? Bisous à tout le monde',
+        'parler Champs Elysées Paris ? monde',
+        [('parler', 'VERB'), ('Champs', 'PROPN'), ('Elysées', 'PROPN'),
+            ('Paris', 'PROPN'), ('?', 'PUNCT'), ('monde', 'NOUN')],
+        ('Champs Elysées Paris')
     ]
+
+    question2 = [
+        'Coucou mon Papounet, toi qui sais tout, c\'est quoi, la Vallée des Saints ?',
+        'sais Vallée Saints ?',
+        [('sais', 'VERB'), ('Vallée', 'NOUN'), ('Saints', 'PROPN'), ('?', 'PUNCT')],
+        ('Vallée Saints')
+        ]
+
+    question3 = [
+        'Salut Fab, tu connais le Musée des Invalides?',
+        'connais Musée Invalides ?',
+        [('connais', 'VERB'), ('Musée', 'NOUN'), ('Invalides', 'PROPN'), ('?', 'PUNCT')],
+        ('Musée Invalides')
+        ]
+
+    questions_tokenize = [
+        (question1[0], question1[1]),
+        (question2[0], question2[1]),
+        (question3[0], question3[1])
+        ]
 
     tag_tokens = [
-        ('parler Champs Elysées Paris ? monde', [
-            ('parler', 'VERB'), ('Champs', 'PROPN'), ('Elysées', 'PROPN'),
-            ('Paris', 'PROPN'), ('?', 'PUNCT'), ('monde', 'NOUN')]),
-        ('sais Vallée Saints ?', [
-            ('sais', 'VERB'), ('Vallée', 'NOUN'), ('Saints', 'PROPN'),
-            ('?', 'PUNCT')]),
-        ('connais Musée Invalides ?', [
-            ('connais', 'VERB'), ('Musée', 'NOUN'), ('Invalides', 'PROPN'),
-            ('?', 'PUNCT')])
-    ]
+        (question1[1], question1[2]),
+        (question2[1], question2[2]),
+        (question3[1], question3[2])
+        ]
 
     filtered_words = [
-        ([('parler', 'VERB'), ('Champs', 'PROPN'), ('Elysées', 'PROPN'),
-            ('Paris', 'PROPN'), ('?', 'PUNCT'), ('monde', 'NOUN')],
-            ('Champs Elysées Paris')),
-        ([('sais', 'VERB'), ('Vallée', 'NOUN'), ('Saints', 'PROPN'),
-            ('?', 'PUNCT')],
-            ('Vallée Saints')),
-        ([('connais', 'VERB'), ('Musée', 'NOUN'), ('Invalides', 'PROPN'),
-            ('?', 'PUNCT')],
-            ('Musée Invalides'))
-    ]
+        (question1[2], question1[3]),
+        (question2[2], question2[3]),
+        (question3[2], question3[3]),
+        ]
