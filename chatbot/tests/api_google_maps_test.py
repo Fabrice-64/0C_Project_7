d4df014015ec6e6813917bdeb478ca099d7af407
Fabrice-64 <fab.jaouen@gmail.com>
2020-08-05 16:55:21 +0200

    First draft test api google maps

---
+++ chatbot/tests/api_google_maps_test.py
@@ -0,0 +1,25 @@
+import requests
+from nose.tools import assert_true
+
+GOOGLE_ROOT = 'https://maps.googleapis.com/maps/api/staticmap'
+
+GOOGLE_PAYLOAD = {
+    'center': 'Hôtel des Invalides',
+    'size': '400x400',
+    'key': 'AIzaSyBIRi_LZmXBjAFfI7L2VlTntLIA9hUThkg',
+    'zoom': '13',
+    'scale': '2',
+    'format': 'jpg',
+    'language': 'fr',
+    'markers': 'size:mid|color:red|Hôtel des Invalides'
+}
+
+def test_connection_ok():
+    response = requests.get(GOOGLE_ROOT, params=GOOGLE_PAYLOAD)
+    assert_true(response.ok)
+
+def test_get_map_ok():
+    response = requests.get(GOOGLE_ROOT, params=GOOGLE_PAYLOAD)
+    response_type = response.headers['Content-Type']
+    assert response_type == 'image/jpeg'
+    assert response.content != None
