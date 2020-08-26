cf8e6cd4d3ff2f0762af86b12cdf1e133ce50b02
Fabrice-64 <fab.jaouen@gmail.com>
2020-08-26 10:16:40 +0200

    Draft of HTML interface up and running

--- chatbot/app/routes.py
+++ chatbot/app/routes.py
@@ -1,24 +1,19 @@
-from flask import render_template, flash, request
+from flask import render_template, request, jsonify
 from app import app
-from app.forms import QuestionForm
-from app.controller.question_parser import QuestionParser
-from app.controller.api_folder.api_wikipedia import WikipediaApi
-from app.controller.api_folder.api_google_maps import GoogleApi
+from app.controller.question_processing import QuestionProcessing
 
 
 @app.route('/')
-@app.route('/question', methods=['GET', 'POST'])
 def question():
-    question = QuestionForm()
-    wikipedia_response=""
-    google_map = ""
-    if question.validate_on_submit():
-        question_to_parse = QuestionParser()
-        request_to_send = question_to_parse.parsing_process(question.question.data)
-        parsed_question = WikipediaApi()
-        exact_location = parsed_question.push_exact_location_name(request_to_send)
-        wikipedia_response = parsed_question.from_location_to_summary(exact_location)
-        google_map = GoogleApi()
-        google_map = google_map.get_map(exact_location)
+    return render_template('question.html')
 
-    return render_template('question.html', question=question, wikipedia_response=wikipedia_response, google_map=google_map)
+
+@app.route('/process_question')
+def process_question():
+    question = request.args.get('question', "None", type=str)
+    processed_question = QuestionProcessing()
+    processed_question = processed_question.question_processing(question)
+    return jsonify(result=processed_question[0],
+                   wikipedia_response=processed_question[1],
+                   google_map=processed_question[2])
+
