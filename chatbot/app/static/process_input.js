83ab2fe29c06ca9c7db3a2900ff7f457c9211fe0
Fabrice-64 <fab.jaouen@gmail.com>
2020-08-30 10:55:38 +0200

    add non valid question to process_input.js

--- chatbot/app/static/process_input.js
+++ chatbot/app/static/process_input.js
@@ -3,8 +3,13 @@ $(function() {
       $.getJSON($SCRIPT_ROOT + '/process_question', {
       question: $('input[name="question"]').val()
       }, function(data){
-        $("#dialogue_box").append('<div class="row align-items-start mb-3"><row_box class=col-4>' + data.question + '</row_box>\
+        if (data.wikipedia_response == "None"){
+          $("#dialogue_box").append('<div class="row align-items-start mb-3"><row_box class=col-4>Je n\'ai pas compris ta question... Peux-tu la reformuler ?</row_box>');
+        } else {
+          $("#dialogue_box").append('<div class="row align-items-start mb-3"><row_box class=col-4>' + data.question + '</row_box>\
           <row_box class=col-4>' + data.wikipedia_response +'</row_box><row_box class=col-4><img src='+ data.google_map +'width="100px" height="100px"></row_box>');
+        };
+        });
         var element = document.getElementById("dialogue_box");
         element.scrollIntoView({block:"end"});
     });
@@ -12,4 +17,3 @@ $(function() {
     var clear_question = document.getElementById("question");
     clear_question.value = "";
   });
-});
